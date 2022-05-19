af317fbeec3fa7b80b675f6657617d61890fb855
Thiago da Conceição Leite <thiago.leite@procfit.com.br>
2022-05-19 10:10:59 -0300

    adição de config ssl reject rejectUnauthorized: false em app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,6 +17,9 @@ import { ItemModule } from './item/item.module';
       database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: (process.env.DB_SYNCHRONIZE === 'true'),
+      ssl:{
+        rejectUnauthorized: false
+      }
     }),
     ItemModule,
   ],
